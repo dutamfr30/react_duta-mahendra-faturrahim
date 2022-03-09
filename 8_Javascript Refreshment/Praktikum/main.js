d11b64efbae3e56c36d932b6fd0de62575e720cb
Duts <90098797+dutamfr30@users.noreply.github.com>
2022-03-09 15:42:59 +0800

    Update main.js

--- 8_Javascript Refreshment/Praktikum/main.js
+++ 8_Javascript Refreshment/Praktikum/main.js
@@ -41,7 +41,7 @@
 const foo = ["Budi", "Sita", "Ayu"];
 const [a, b, c] = foo;
 
-console.log("Jawaban Nomor 3")
+console.log("Jawaban Nomor 3");
 console.log("a = " + a);
 console.log("b = " + b);
 console.log("c = " + c);
@@ -50,19 +50,19 @@ console.log("c = " + c);
 let bdays = ['10-17','05-19','20-19'];
 bdays = ['10/17', '05/19', '20/19'];
 
-console.log("Jawaban Nomor 4")
+console.log("Jawaban Nomor 4");
 console.log(bdays);
 
 //Jawaban Nomor 5
 let value = [1,2,3,4,5,6];
 let kali2 = value.map(x => x * 2);
 
-console.log("Jawaban Nomor 5")
-console.log(kali2)
+console.log("Jawaban Nomor 5");
+console.log(kali2);
 
 //Jawaban Nomor 6
 let arr = [1.5, 2.56, 5.1, 12.33];
 let bulat = arr.map(Math.round);
 
-console.log("Jawaban Nomor 6")
-console.log(bulat)
+console.log("Jawaban Nomor 6");
+console.log(bulat);
