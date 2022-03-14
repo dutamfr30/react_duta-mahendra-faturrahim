fa98d8507bceb39415beac1d7703be61b32d6afc
Duts <dutamfr@gmail.com>
2022-03-14 22:49:34 +0800

    Add file react
    Add 10-Introduction-React

---
+++ 10_Introduction_React/Praktikum/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import Home from "./pages/home";
+import Contact_us from './pages/contact_us'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <Contact_us />
+    </div>
+  );
+}
+
+export default App;
